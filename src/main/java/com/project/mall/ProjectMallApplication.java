d2030ce88de5a65dc49cf942e1f9b36be661621a
vulpera-fox <vulpera.yeowoo@gmail.com>
2023-08-04 19:24:07 +0900

    first commit

---
+++ src/main/java/com/project/mall/ProjectMallApplication.java
@@ -0,0 +1,13 @@
+package com.project.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMallApplication.class, args);
+	}
+
+}
