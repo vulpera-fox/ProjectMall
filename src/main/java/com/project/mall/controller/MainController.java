952c1feb826921ac6e15b4caf98d883ccfd8d09a
vulpera-fox <vulpera.yeowoo@gmail.com>
2023-08-05 10:58:16 +0900

    basic templates

---
+++ src/main/java/com/project/mall/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.project.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	
+	@RequestMapping("/")
+	public String main() {
+		
+		
+		return "/main";
+		
+	}
+
+}
